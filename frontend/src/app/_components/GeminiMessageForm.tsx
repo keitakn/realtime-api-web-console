9cb59e89e3f8ce90138f4e848e933bebe532ee66
keitakn <keita.koga.work@gmail.com>
2024-12-21 03:41:08 +0900

    :art: 音声ファイルを送信する為にマイクアイコン付きのフォームに変更

--- frontend/src/app/_components/GeminiMessageForm.tsx
+++ frontend/src/app/_components/GeminiMessageForm.tsx
@@ -150,25 +150,17 @@ export function GeminiMessageForm(): JSX.Element {
       </div>
 
       <form onSubmit={sendMessage}>
-        <label htmlFor="chat-input" className="sr-only">Enter prompt</label>
-        <div className="flex gap-x-2">
-          <input
-            id="chat-input"
-            type="text"
-            value={input}
-            className="w-full rounded-lg border border-slate-300 bg-slate-200 p-3 text-sm text-slate-800 shadow-md focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 sm:text-base dark:border-slate-200/10 dark:bg-slate-800 dark:text-slate-200 dark:placeholder:text-slate-400 dark:focus:border-blue-600"
-            placeholder="Enter prompt"
-            onChange={e => setInput(e.target.value)}
-            required
-          />
+        <label htmlFor="chat-input" className="sr-only">Enter your prompt</label>
+        <div className="relative">
           <button
-            type="submit"
-            className="rounded-lg border border-transparent bg-blue-600 px-3 py-1 text-slate-200 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            type="button"
+            className="absolute inset-y-0 left-0 flex items-center pl-3 text-slate-500 hover:text-blue-600 dark:text-slate-400 dark:hover:text-blue-600"
           >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
               className="size-5"
               viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
               strokeWidth="2"
               stroke="currentColor"
               fill="none"
@@ -176,13 +168,32 @@ export function GeminiMessageForm(): JSX.Element {
               strokeLinejoin="round"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M10 14l11 -11"></path>
               <path
-                d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5"
+                d="M9 2m0 3a3 3 0 0 1 3 -3h0a3 3 0 0 1 3 3v5a3 3 0 0 1 -3 3h0a3 3 0 0 1 -3 -3z"
               >
               </path>
+              <path d="M5 10a7 7 0 0 0 14 0"></path>
+              <path d="M8 21l8 0"></path>
+              <path d="M12 17l0 4"></path>
             </svg>
-            <span className="sr-only">Enter prompt</span>
+            <span className="sr-only">Use voice input</span>
+          </button>
+          <textarea
+            id="chat-input"
+            className="block w-full resize-none rounded-xl border-none bg-slate-200 p-4 pl-10 pr-20 text-sm text-slate-900 focus:outline-none focus:ring-2 focus:ring-blue-600 sm:text-base dark:bg-slate-800 dark:text-slate-200 dark:placeholder:text-slate-400 dark:focus:ring-blue-600"
+            placeholder="Enter your prompt"
+            onChange={e => setInput(e.target.value)}
+            rows={1}
+            required
+          >
+          </textarea>
+          <button
+            type="submit"
+            className="absolute bottom-2 right-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-slate-200 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:text-base dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Send
+            {' '}
+            <span className="sr-only">Send message</span>
           </button>
         </div>
       </form>
